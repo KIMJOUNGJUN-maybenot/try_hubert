2622a90f186027cb9ba124b74bb8200a900b4e8d
KIMJOUNGJUN-maybenot <ingiproject3@gmail.com>
2024-12-10 10:21:29 +0900

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -3,10 +3,17 @@ dependencies = ["torch", "torchaudio", "transformers"]
 
 from transformers import HubertForCTC
 
-def hubert_ee(model_path: str):
+from transformers import HubertForCTC
+
+def hubert_ee(model_path: str, vocab_size=50, ee_layers=None):
     """
     Load a huggingface HubertForCTC model from a local directory.
     """
     model = HubertForCTC.from_pretrained(model_path)
+    # `vocab_size`와 `ee_layers`를 처리하는 로직 추가
+    if ee_layers:
+        model.config.layer_drop = ee_layers
+    model.config.vocab_size = vocab_size
     model.eval()
     return model
+
